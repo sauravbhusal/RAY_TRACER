ffc813b9285169c4778fe285187dee14f48dbd06
sauravbhusal <bhusalsaurav0@gmail.com>
2025-05-30 13:15:48 +0545

    added a loader to visualize the process

--- practice_codes/plain_gradient.cpp
+++ practice_codes/plain_gradient.cpp
@@ -7,11 +7,13 @@ int main() {
 
     //render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
     for(int j=0; j<image_height; j++){
+        std::clog << "\rScanLines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i=0; i<image_width; i++){
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
+            auto r = double(j) / (image_width-1);
+            auto g = 0;
+            auto b = double(i) / (image_height-1);
 
             int ir = int(255.999 * r);
             int ig = int(255.999 * g);
