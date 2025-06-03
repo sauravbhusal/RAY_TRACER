ad6596097f190ad326aaca36322440d36404859a
sauravbhusal <bhusalsaurav0@gmail.com>
2025-06-03 15:18:10 +0545

    same result as practice code but with the headers

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "vec3.h"
+#include "color.h"
+
+
+int main() {
+    //image
+    int image_width = 256;
+    int image_height = 256;
+
+
+    //render image
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for(int j=0; j<image_width; j++) {
+        std::clog << "\rSccanlines remaining: " << (image_height-j) << ' ' << std::flush;
+        for (int i=0; i<image_height; i++) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                      \n";
+}
+
