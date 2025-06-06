19bb1e79845bfd0192d4f7ee892cb6114d0a0b89
sauravbhusal <bhusalsaurav0@gmail.com>
2025-06-06 16:07:51 +0545

    removed clutter and added code to generate sphere in the space

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,20 @@
 #include "color.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2*dot(oc,r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return(discriminant >= 0);
+}
+
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1,0,0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y()+1.0);
     return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5, 0.7, 1.0);
@@ -40,9 +53,9 @@ int main() {
     //render image
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for(int j=0; j<image_width; j++) {
+    for(int j=0; j<image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height-j) << ' ' << std::flush;
-        for (int i=0; i<image_height; i++) {
+        for (int i=0; i<image_width; i++) {
             auto pixel_center = pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
